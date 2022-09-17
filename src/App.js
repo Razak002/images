3d9e3736f78ba6f8284f8d2bcbdf8f6625bb091a
unknown <Aliyu Abdulrazak>
2022-09-17 04:15:39 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Components/Header';
+import Slider from './Components/Slider/Slider'
+
+
+function App() {
+  return (
+    <>
+    <Header />
+      <Slider />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(){
+    return(
+        <div className="mci">
+        <h1> MCI COODING TEAM SET OF 2022</h1>
+        <h2><i>"Good byes are not forever it means shall we meet again"</i></h2>
+   
+    </div>
+    )
+}
+
+export default Header
